5d9d97a4109135e184b37b06a56057481e027f53
MajesNix <majesnix@users.noreply.github.com>
2024-03-01 16:54:15 +0100

    feat: filter out all urls that contain dcl.re

--- src/main/scala/com/majesnix/sus/models/UrlModels.scala
+++ src/main/scala/com/majesnix/sus/models/UrlModels.scala
@@ -5,5 +5,5 @@ case class CreateShortUrlResponse(short: String)
 case class ShortUrl(short: String, url: String)
 
 object Url {
-  def valid(url: Url): Boolean = url.url.matches("(https|http|ftp).*")
+  def valid(url: Url): Boolean = url.url.matches("(https|http|ftp).*") && url.url.matches("^((?!dcl\\.re).)*$")
 }
